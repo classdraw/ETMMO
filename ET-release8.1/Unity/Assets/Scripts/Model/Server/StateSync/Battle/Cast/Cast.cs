4b24bfad648275a714acef26aa388a55614a2d46
jiayanyin <365674221@qq.com>
2026-06-04 08:17:00 +0800

    cast主键

--- ET-release8.1/Unity/Assets/Scripts/Model/Server/StateSync/Battle/Cast/Cast.cs
+++ ET-release8.1/Unity/Assets/Scripts/Model/Server/StateSync/Battle/Cast/Cast.cs
@@ -1,8 +1,28 @@
+using System.Collections.Generic;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace ET.Server
 {
+    [ChildOf(typeof(CastComponent))]
     public class Cast:Entity,IAwake<int>,IDestroy
     {
-        public int ConfigId;
+        public int ConfigId;//cast表id
+
+        [BsonIgnore]
+        public CastConfig Config
+        {
+            get
+            {
+                return CastConfigCategory.Instance.Get(this.ConfigId);
+            }
+        }
+        
+        [BsonIgnore]
+        public EntityRef<Unit> Caster;//释放者
+        [BsonIgnore]
+        public List<long> Targets = new List<long>();//技能目标
+
+        public long StartTime;//技能开始时间
     }
 }
 
---
+++ ET-release8.1/Unity/Assets/Scripts/Model/Server/StateSync/Battle/Cast/CastComponent.cs
@@ -0,0 +1,11 @@
+namespace ET.Server
+{
+    /// <summary>
+    /// 技能释放主键
+    /// </summary>
+    [ComponentOf(typeof(Unit))]
+    public class CastComponent:Entity,IAwake,IDestroy
+    {
+    
+    } 
+}
