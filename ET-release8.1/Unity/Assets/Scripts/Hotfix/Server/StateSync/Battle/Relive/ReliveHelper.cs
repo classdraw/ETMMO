6e2dfd6a5355112518b79dc6a34b9a705c5a5dbf
jiayanyin <365674221@qq.com>
2026-06-28 17:30:23 +0800

    复活

--- ET-release8.1/Unity/Assets/Scripts/Hotfix/Server/StateSync/Battle/Relive/ReliveHelper.cs
+++ ET-release8.1/Unity/Assets/Scripts/Hotfix/Server/StateSync/Battle/Relive/ReliveHelper.cs
@@ -1,6 +1,64 @@
-namespace ET.Server;
+using Unity.Mathematics;
 
-public class ReliveHelper
+namespace ET.Server
 {
-    
+    [FriendOf(typeof(ReliveComponent))]
+    public static class ReliveHelper
+    {
+        /// <summary>
+        /// 是否存活
+        /// </summary>
+        /// <param name="unit"></param>
+        /// <returns></returns>
+        public static bool IsAlive(this Unit unit)
+        {
+            return unit.GetComponent<ReliveComponent>()?.Alive??true;
+        }
+
+        public static int OnSiteRelive(this Unit unit)
+        {
+            if (unit.IsAlive())
+            {
+                return ErrorCode.ERR_Relive_Alive;
+            }
+            //复活
+            unit.DoRelive(unit.Position,1.0f);//原地复活 后面再传送
+            return ErrorCode.ERR_Success;
+        }
+
+        public static int PointRelive(this Unit unit,float pos)
+        {
+            if (unit.IsAlive())
+            {
+                return ErrorCode.ERR_Relive_Alive;
+            }
+            unit.DoRelive(pos,1.0f);
+            return ErrorCode.ERR_Success;
+        }
+
+        public static void DoRelive(this Unit unit,float3 pos,float hpRate)
+        {
+            if (unit.IsAlive())
+            {
+                return;
+            }
+
+            unit.Position = pos;
+            NumericComponent numericComponent=unit.GetComponent<NumericComponent>();
+            if (numericComponent!=null)
+            {
+                var maxHp = numericComponent[NumericType.MaxHp];
+                var needHp = maxHp * hpRate;
+                numericComponent[NumericType.HpBase] = math.clamp((int)needHp, 1, maxHp);
+            }
+            ReliveComponent reliveComponent=unit.GetComponent<ReliveComponent>();
+            if (reliveComponent!=null)
+            {
+                reliveComponent.Alive = true;
+            }
+
+            
+        }
+    }
 }
+
